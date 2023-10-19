e9729670ed387728661f83451abf1f8120ead288
belyshevaoksanai <belyshevaoksanai@yandex.ru>
2023-10-19 15:38:54 +0300

    fix style for error

--- src/components/Input/Error/index.ts
+++ src/components/Input/Error/index.ts
@@ -1,12 +1,17 @@
+import TypeWithClass from '../../../models/block-helpers';
 import Block from '../../../utils/block';
+import classes from './error.module.scss';
 
 interface InputErrorProps {
   text: string;
 }
 
-class InputError extends Block<InputErrorProps> {
+class InputError extends Block<TypeWithClass<InputErrorProps>> {
   constructor(props: InputErrorProps) {
-    super('span', props);
+    super('span', {
+      ...props,
+      class: classes.error,
+    });
   }
 
   render() {
