222cf457003bb955e75683c205a9e6e4859be599
belyshevaoksanai <belyshevaoksanai@yandex.ru>
2023-10-19 18:05:05 +0300

    fix

--- src/pages/Registration/index.ts
+++ src/pages/Registration/index.ts
@@ -1,5 +1,5 @@
 import Block from '../../utils/block';
-import classes from './Registration2.module.scss';
+import classes from './Registration.module.scss';
 import RegistrationForm from './RegistrationForm';
 
 class Registration extends Block {
